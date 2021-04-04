7714ac20b9d677bd47f7c108abeb344dc3aed02b
Shreyansh jain <78850912+shreyansh1406@users.noreply.github.com>
2021-04-04 17:05:57 +0530

    Update 8.kt

--- main/java/com/example/assessment/8.kt
+++ main/java/com/example/assessment/8.kt
@@ -1,2 +1,21 @@
 package com.example.assessment
 
+import java.util.*
+
+fun main()
+{
+    var input = Scanner(System.`in`)
+    println("Enter the nth number")
+    var n = input.nextInt()
+    var c=0
+    for(i in 1.. n) {
+        for (j in 1..i) {
+            if (i%j==0)
+                c++
+        }
+        if(c==2)
+            println(i)
+        c=0
+    }
+
+}
