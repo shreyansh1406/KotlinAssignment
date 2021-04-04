d5a4506779e862a83c7edc431ffb37228d7cf6bc
Shreyansh jain <78850912+shreyansh1406@users.noreply.github.com>
2021-04-04 22:09:12 +0530

    Solutions

--- main/java/com/example/assessment/5.kt
+++ main/java/com/example/assessment/5.kt
@@ -1,2 +1,17 @@
 package com.example.assessment
 
+import java.util.*
+
+fun main(args: Array<String>){
+    val sc = Scanner(System.`in`)
+
+    print("Enter the nth Number : ")
+    val n: Int = sc.nextInt()
+    val myLambda: (Int) -> Unit= {s: Int -> println(s) } //lambda function
+    fact(n,myLambda)
+}
+fun fact(a: Int, mylambda: (Int) -> Unit ){
+    var n=1//high level function lambda as parameter
+    for(i in 1..a){n=n*i}
+    mylambda(n) 
+}
