052687f15fa2397139d154835b1e2e7f9b1aaa4d
Shreyansh jain <78850912+shreyansh1406@users.noreply.github.com>
2021-04-05 09:04:54 +0530

    Solutions

--- main/java/com/example/assessment/5.kt
+++ main/java/com/example/assessment/5.kt
@@ -2,16 +2,19 @@ package com.example.assessment
 
 import java.util.*
 
-fun main(args: Array<String>){
+fun main()
+{
+    val factorial : (Int) -> Unit = { number ->
+        var f : Int = 1
+        for(i in 1..number)
+        {
+            f *= i
+        }
+        println(f)
+    }
     val sc = Scanner(System.`in`)
 
     print("Enter the nth Number : ")
     val n: Int = sc.nextInt()
-    val Lambda: (Int) -> Unit= {s: Int -> println(s) }
-    fact(n,Lambda)
-}
-fun fact(a: Int, lambda: (Int) -> Unit ){
-    var n=1
-    for(i in 1..a){n=n*i}
-    lambda(n)
+    factorial(n)
 }
